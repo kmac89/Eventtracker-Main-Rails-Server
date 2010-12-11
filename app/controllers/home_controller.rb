3491349bc31a8979e867ed1d5f1e19648376b389
Alex Degtiar <adegtiar@berkeley.edu>
2010-12-10 19:59:06 -0800

    Ian's additions: draft ip_update controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,23 @@ class HomeController < ApplicationController
   def index
   end
 
+  def update_ip
+    check_errors do
+      ip = params[:ip]
+      user_id = params[:user_id]
+
+      # is_valid? should actually be a validator on the User model
+      throw "Invalid IP address" unless is_valid?(id)
+
+      user = User.first(user_id)
+      user.ip = ip
+
+      # However active_record says that things were saved correctly should be checked
+      user.save
+
+      # We don't actually want to render a view here, so don't do that...
+      "Thanks for the update"
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def check_errors
+    begin
+      return yeild
+    rescue Exception => e
+      return "<html><body>So, sorry, you suck.</body></html>"
+    end
+  end
 end

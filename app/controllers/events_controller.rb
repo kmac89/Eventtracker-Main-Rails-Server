758f3c1b80ac9e36a0059da6db410ef7a32bc318
Alex Degtiar <adegtiar@berkeley.edu>
2011-02-05 13:04:09 -0800

    Added deleting and updating events view phone functionality.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -74,24 +74,43 @@ class EventsController < ApplicationController
   # DELETE /events/1
   # DELETE /events/1.xml
   def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
+    errors = false
+    if params[:id] then
+      @event = Event.find(params[:id])
+    else 
+      # delete by uuid and device uuid
+      uuid = params[:UUIDOfEvent]
+      user_uuid = params[:UUIDOfDevice]
+      if uuid and user_uuid then
+        @event = Event.find_by_uuid(uuid)
+        # user_uuid must match
+        errors = @event.user_id == User.find_by_uuid(user_uuid)
+      else
+        # must pass uuid and device uuid
+        errors = true
+      end
+    end
+    @event.destroy unless errors
 
     respond_to do |format|
-      format.html { redirect_to(events_url) }
-      format.xml  { head :ok }
+      if !errors then
+        format.html { redirect_to(events_url) }
+        format.xml  { head :ok }
+      else
+        format.html { redirect_to(events_url, :notice => 'Could not destroy event') }
+        format.xml  { head :ok }
+      end
     end
   end
 
-  # POST /event/upload
-  # POST /event/upload.xml
+  # POST /events/upload
+  # POST /events/upload.xml
   def upload
-    @event = Event.new
+    @event = Event.find_or_create_by_uuid(params[:UUIDOfEvent])
     user = User.find_by_uuid(params[:UUIDOfDevice])
-    @event.user_id = @user.id 
+    @event.user_id = user.id unless user.nil?
     @event.uuid = params[:UUIDOfEvent]
     @event.content = params[:EventData]
-
 
     respond_to do |format|
       if @event.save

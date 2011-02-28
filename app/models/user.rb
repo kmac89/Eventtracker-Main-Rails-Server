688d42543520d34b64066e204c23c855dd1cb750
Alex Degtiar <adegtiar@berkeley.edu>
2011-02-28 01:22:32 -0800

    Added authentication and account registration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.validate_email_field = false
+    c.require_password_confirmation = false
+    c.login_field = :phone_number
   end
 
   has_many :events, :dependent => :destroy
 
   validates :uuid, :presence => true
   validates :phone_number, :presence => true
-  validates :password_hash, :presence => true
-
-#  def to_param
-#    phone_number
-#  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110227084456) do
+ActiveRecord::Schema.define(:version => 20110228090031) do
 
   create_table "events", :force => true do |t|
     t.string   "uuid",       :limit => 36
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(:version => 20110227084456) do
     t.string   "phone_number"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_hash"
     t.string   "crypted_password"
     t.string   "password_salt"
     t.string   "persistence_token"

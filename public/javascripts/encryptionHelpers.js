e8d7be14fe87f2fa804c0d1e5f1ab12e0f79292f
Kyle <kmac89@berkeley.edu>
2011-06-10 20:06:42 -0700

    Using local storage rather than session storage now. This is because session storage is maintained separately for each tab, so it conflicts with the cookie authentication

--- public/javascripts/encryptionHelpers.js
+++ public/javascripts/encryptionHelpers.js
@@ -5,13 +5,13 @@
 var encryptionHelpers ={
 
 	unencrypt:function(encryptedText) {
-	  return GibberishAES.dec(encryptedText,sessionStorage.pwd);
+	  return GibberishAES.dec(encryptedText,localStorage.pwd);
         },
 	
 	unencryptAll:function(encryptedArray) {
 	var unencryptedArray=new Array(); 
 	for(var i=0; i < encryptedArray.length; i++){
-		unencryptedArray[i]=GibberishAES.dec(encryptedArray[i],sessionStorage.pwd);
+		unencryptedArray[i]=GibberishAES.dec(encryptedArray[i],localStorage.pwd);
 	}
 	return unencryptedArray;
    }

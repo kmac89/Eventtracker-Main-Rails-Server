0f3add579796e575962e31091d31c9e6db8b3f78
Alex Degtiar <adegtiar@berkeley.edu>
2011-07-30 14:12:16 -0700

    Changed event content from string to text

---
+++ db/migrate/20110730210530_change_event_content_to_text.rb
@@ -0,0 +1,9 @@
+class ChangeEventContentToText < ActiveRecord::Migration
+  def self.up
+    change_column(:events, :content, :text)
+  end
+
+  def self.down
+    change_column(:events, :content, :string)
+  end
+end

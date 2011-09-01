740537ec38c33a3ce0c8a66f4b8acb50f8dfa6ed
Kyle MacNamara <kmac89@berkeley.edu>
2011-08-31 22:04:11 -0700

    Customized the login error messages.

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,6 +1,6 @@
 class UserSession < Authlogic::Session::Base
 
-  generalize_credentials_error_messages true
+  generalize_credentials_error_messages "Wrong phone number/password combination" 
 
   def to_key
      new_record? ? nil : [ self.send(self.class.primary_key) ]

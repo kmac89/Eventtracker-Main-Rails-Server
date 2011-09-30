7a57f3528828796d29c3af0955c49dffce0c9f67
Kyle MacNamara <kmac89@berkeley.edu>
2011-09-29 17:58:27 -0700

    Deleted more unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ MainRailsServer::Application.routes.draw do
 	match ":phone_number/table"     => "events#table"
   end
 
+  root :to => "events#calendar"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -78,7 +80,6 @@ MainRailsServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "events#calendar"
 
   # See how all your routes lay out with "rake routes"
 
